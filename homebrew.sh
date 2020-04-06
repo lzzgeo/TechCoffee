ab14051bbc6cd6c7bc876258f680d3a4e630deea
sam <5943916+lzzgeo@users.noreply.github.com>
2020-04-07 07:45:47 +0800

    Create homebrew.sh

---
+++ homebrew.sh
@@ -0,0 +1,20 @@
+# https://brew.sh/
+
+brew update
+
+brew ls
+
+brew install <xxx>
+brew uninstall <xxx>
+
+
+brew outdated
+
+
+brew upgrade
+brew upgrade <xxxx>
+
+
+brew search  <xxx>
+
+brew info <xxxx>
